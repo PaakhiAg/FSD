8aa58038f8fda2b11b66e52fccd5980b21a58bee
PaakhiAg <agarwalpaakhi2005@gmail.com>
2026-02-12 15:19:17 +0530

    First commit

---
+++ nodetest/index.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');   // to read image file
+
+const myserver = http.createServer((req, res) => {
+
+    if (req.url == '/') {
+        res.end("ABES College");
+    }
+
+    else if (req.url == "/about") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(`
+            <h1>This is my About Page</h1>
+            <img src="/image" width="900">
+        `);
+    }
+
+    else if (req.url == "/image") {
+        fs.readFile("image.png", (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'image/png' });
+            res.end(data);
+        });
+    }
+
+    else if (req.url == "/contact") {
+        res.end("304 tulip grace");
+    }
+
+    else {
+        res.end("404 page is not found!");
+    }
+});
+
+myserver.listen(8000, () => console.log("server is running"));
